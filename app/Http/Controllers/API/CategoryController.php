7bfbe76315611db916f5b090651e2782f47c389f
Elsynt11 <elsynoh11@gmail.com>
2025-04-24 20:32:58 -0600

    Add avaible products info to the show method of CategoryController

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -58,6 +59,7 @@ class CategoryController extends Controller
         try 
         {
             $category = Category::find($id);
+            $category->available_products = $category->products->sum('quantity');
 
             if(!$category) 
             {
